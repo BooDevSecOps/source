81f7784c99858aaed58c0a41260970cf4d047cff
BooDevSecOps <boo.fi2023@gmail.com>
2024-09-05 19:35:25 +0800

    Add files via upload

---
+++ docker-file/restart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#ini restart aapanel
+/www/server/mysql/scripts/mysql_install_db --user=mysql
+/etc/init.d/mysqld start
+/etc/init.d/php-fpm-74 start
+/etc/init.d/php-fpm-82 start
+/etc/init.d/redis start
+bt 1
+#end restart aapanel
